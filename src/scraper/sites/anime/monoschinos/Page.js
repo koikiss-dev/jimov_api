52119506418e831101fc2e7804bfb410158917c8
Zukaritasu <perezjhonlenno29@gmail.com>
2023-02-09 20:26:47 -0400

    Irrelevant change

--- src/scraper/sites/anime/monoschinos/Page.js
+++ src/scraper/sites/anime/monoschinos/Page.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { Episode } from "../../../../utils/episode";
 
 const PageInfo = {
-    url: 'https://monoschinos2.com/' // url page
+    url: 'https://monoschinos2.com' // url page
 }
 
 /**
@@ -41,7 +42,7 @@ async function getLastEpisodes() {
         $('div.heroarea div.heroarea1 div.row').children().each(async (i, element) => {
             if ($(element).children().length != 0) {
                 let anime = [ null ];
-                episodes.push({
+                episodes.push(new Episode = {
                     name:    $(element).find('h2.animetitles').text().trim(),
                     image:   $(element).find('div.animeimgdiv img.animeimghv').attr('data-src'),
                     url:     $(element).find('a').attr('href'),
@@ -49,7 +50,6 @@ async function getLastEpisodes() {
                     video:   await getEpisodeServers($(element).find('a').attr('href'), anime),
                     anime:   anime[0]
                 });
-                console.log(episodes[episodes.length - 1])
             }
         });
         return episodes;
@@ -59,6 +59,45 @@ async function getLastEpisodes() {
     return null;
 }
 
-await getLastEpisodes()
+/**
+ * 
+ * @param {*} url 
+ * @returns 
+ */
+async function getAnimeInfo(url) {
+    let animes = [];
+    const $ = cheerio.load((await axios.get(url)).data);
+    return {
+        name: $('div.chapterdetails').find('h1').text(),
+        url: url,
+        description: $('div.chapterdetails p.textComplete').text().trim(),
+        image: {
+            banner: $('div.herobg img').attr('src'),
+            url: $('div.chapterpic img').attr('src')
+        }
+    };
+}
 
-export default { getLastEpisodes };
+/**
+ * 
+ * @returns 
+ */
+async function getLastAnimes() {
+    try {
+        let animes = [];
+        const $ = cheerio.load((await axios.get(`${ PageInfo.url }/emision`)).data);
+        $('div.heroarea div.heromain div.row').children().each(async (i, element) => {
+            if ($(element).find('a').attr('title') != undefined) {
+                animes.push(getAnimeInfo($(element).find('a').attr('href')));
+            }
+        });
+        return animes;
+    } catch (error) {
+        console.log(error);
+    }
+    return null;
+}
+
+await getLastAnimes()
+
+export default { getLastEpisodes, getLastAnimes };
