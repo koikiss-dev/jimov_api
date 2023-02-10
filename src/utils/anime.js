5e7f3634b7b1bafa77ed521736dc7795ff8c46e5
Zukaritasu <perezjhonlenno29@gmail.com>
2023-02-09 21:18:34 -0400

    The structure of the general information of an anime has been improved.

--- src/utils/anime.js
+++ src/utils/anime.js
@@ -20,27 +20,96 @@
  * SOFTWARE.
  */
 
+import { Episode } from "./episode";
+import { Image } from "./image";
+
 /**
+ * Specifies the anime climatic station
  * @readonly
  * @enum {String}
  */
-export const WeatherStation = {
+export const ClimaticStation = {
 	Summer: Symbol('summer'),
 	Autumn: Symbol('autumn'),
 	Winter: Symbol('winter'),
 	Spring: Symbol('spring'),
 }
 
+/**
+ * Anime chronology
+ * @author Zukaritasu
+ */
+export class Chronology {
+	/**
+	 * The name of the anime
+	 * @type {string}
+	 */
+	name;
+	/**
+	 * The URL of the anime. It can also refer to movies, ovas and onas.
+	 * @type {string}
+	 */
+	url;
+	/**
+	 * The cover image of the anime
+	 * @type {string}
+	 */
+	image;
+}
+
+/**
+ * General information about an anime. The class contains information
+ * that can be found on any anime website.
+ * @author Zukaritasu
+ */
 export class Anime {
-	name       = String()
-	url        = String()
-	synopsis   = String()
-	year       = Number()
+	/**
+	 * The name of the anime
+	 * @type {string}
+	 */
+	name;
+	/**
+	 * The URL of the anime
+	 * @type {string}
+	 */
+	url;
+	/**
+	 * Anime synopsis
+	 * @type {string}
+	 */
+	synopsis;
+	/**
+	 * The cover and banner of the anime
+	 * @type {Image}
+	 */
+	image;
+	/**
+	 * The year the anime was released
+	 * @type {number}
+	 */
+	year;
+	/**
+	 * Anime genres. Genres are defined by a text string in English or
+	 * Spanish depending on the location
+	 * @type {string[]}
+	 */
 	genres     = []
-    /** Use WeatherStation */
-	station;
-    /** Class Anime element */
-	chronology = []
-    /** Class Episode element */
+	/**
+	 * Climatic station from the anime. If the station is not defined then
+	 * the default value is null
+	 * @type {(ClimaticStation | null)}
+	 * @default null
+	 */
+	station    = null;
+	/**
+	 * Specifies the chronological order of the anime
+	 * @type {Chronology[]}
+	 */
+	chronology;
+	/**
+	 * Anime episodes available. If the anime is on air it will only show the
+	 * available episodes, and if it is not on air it will show all the episodes.
+	 * @type {Episode[]}
+	 */
 	episodes   = []
 }
