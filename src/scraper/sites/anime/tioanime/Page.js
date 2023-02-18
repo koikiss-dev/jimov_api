86d3ec089a17ba7fb4ae87b1dddb7658449bb47f
Zukaritasu <perezjhonlenno29@gmail.com>
2023-02-18 07:21:29 -0400

    The scraping of the TioAnime site is finished, we still need to include your filter.

--- src/scraper/sites/anime/tioanime/Page.js
+++ src/scraper/sites/anime/tioanime/Page.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { Anime, Episode, Image, Chronology, EpisodeServer } from "../../../../utils/schemaProviders.js";
 
 const PageInfo = {
     url: 'https://tioanime.com' // url page
@@ -7,123 +8,86 @@ const PageInfo = {
 
 /**
  * 
- * @param {*} params 
- */
-async function getVideoServers(url) {
-    'use strict'
-    const $ = cheerio.load((await axios.get(PageInfo.url + url)).data);
-
-    const scripts    = $('script').get();
-    const script     = $(scripts[scripts.length - 1]).text().trim();
-    const script_src = script.substring(0, script.indexOf('$(document)'));
-
-    eval?.(script_src);
-    eval(script_src);
-    return globalThis.videos;
-}
-
-/**
- * 
  * @param {*} $ 
+ * @return {Chronology[]}
  */
 function getAnimeChronology($) {
     let chrono_list = [];
     $('section.w-history ul.list-unstyled li').each((i, element) => {
-        chrono_list.push({
-            name:  $(element).find('h3.title').text(),
-            image: PageInfo.url + $(element).find('figure.fa-play-circle img').attr('src'),
-            url:   PageInfo.url + $(element).find('div.media-body a').attr('href'),
-            year:  $(element).find('div.meta span.year').text().substring(0, 4),
-            type:  $(element).find('div.thumb span.anime-type-peli').text()
-        });
+		// The chronological anime has to access its year and type as extra
+		// information that is not included in the Chronology class
+		chrono_list.push(new Chronology(
+				$(element).find('h3.title').text(), 
+				PageInfo.url + $(element).find('div.media-body a').attr('href'), 
+				PageInfo.url + $(element).find('figure.fa-play-circle img').attr('src')
+			));
     });
     return chrono_list;
 }
 
 /**
  * 
- * @param {*} url 
+ * @param {string} url anime episode url
+ * @return {EpisodeServer[]}
  */
-async function getAnimeInfo(url) {
+async function getEpisodeServers(url) {
+	'use strict'
+	let servers = []
     const $ = cheerio.load((await axios.get(PageInfo.url + url)).data);
-
-    const scripts    = $('script').get();
-    const script     = $(scripts[scripts.length - 1]).text().trim();
-    const script_src = script.substring(0, script.indexOf('$(document)'));
-
-    let execute = function (code) {
-        'use strict'
-        eval?.(code);
-        eval(code);
-
-        return {
-            // get variables by script source code
-            _anime_info: globalThis.anime_info,
-            _episodes: globalThis.episodes
-        };
-    };
-
-    const result = execute(script_src);
-    let episodes = [];
-
-    result._episodes.forEach(/* number */ element => {
-        episodes.push({
-            name:   result._anime_info[2],
-            image: `${ PageInfo.url }/uploads/thumbs/${ result._anime_info[0] }.jpg`,
-            url:   `${ PageInfo.url }/ver/${ result._anime_info[1] }-${ element }`
-        });
-    });
-
-   return {
-        name:        $('div.container h1.title').text(),
-        image:       PageInfo.url + $('div.container div.thumb figure img').attr('src'),
-        url:         PageInfo.url + url,
-        description: $('div.container p.sinopsis').text().trim(),
-        genres:      getAnimeGenres($, $('div.container p.genres span')),
-        year:        $('div.meta span.year').text().substring(0, 4),
-        type:        $('div.meta span.anime-type-peli').text(),
-        season:      $('div.meta span.fa-snowflake').text().split('\n')[0],
-        episodes:    episodes,
-        active:      $('div.thumb a.status').text() === 'En emision',
-        chronology:  getAnimeChronology($)
-    };
-}
-
-/**
- * finished!
- */
-async function getLastAnimes() {
-    try {
-        let animes = [];
-        const $ = cheerio.load((await axios.get(PageInfo.url)).data);
-        $('div.container section ul.list-unstyled.row li').each(async (i, element) => {
-            const anime_url = $(element).find('article.anime a').attr('href');
-            if (anime_url != undefined) {
-                animes.push(await getAnimeInfo(anime_url));
-            }
-        });
-        return animes;
-    } catch (error) {
-        console.log(error);
+	const script = $($('script').get().pop()).text().trim();
+	try {
+		// The variable 'videos' of the script is accessed
+		eval?.(script.substring(0, script.indexOf('$(document)')));
+		eval(script.substring(0, script.indexOf('$(document)')));
+		for (let i = 0; i < globalThis.videos.length; i++) {
+			servers.push(new EpisodeServer(globalThis.videos[i][0], 
+				globalThis.videos[i][1].replace('\\', '')
+			));
+		}
+	} catch (error) {
+        
     }
-    return null;
+	return servers;
 }
 
 /**
  * 
- * @param {*} title 
- * @returns 
+ * @param {Element} $ element anime
+ * @return {Episode[]}
  */
-function getEpisodeInfo(title) {
+async function getAnimeEpisodes(data) {
+	let __episodes = [];
+	data.episodes.forEach(episode_number => {
+		let episode     = new Episode();
+		episode.name    = `${data.info[2]} Capitulo ${episode_number}`;
+		episode.image   = `/uploads/thumbs/${data.info[0]}.jpg`;
+		episode.url     = `/ver/${data.info[1]}-${episode_number}`;
+		episode.number  = episode_number;
+        __episodes.push(episode);
+	});
+	return __episodes;
+}
+
+/**
+ * 
+ * @param {*} $ 
+ * @param {*} elements
+ * @returns {Episode}
+ */
+function getEpisode($, element) {
+	const title   = $(element).find('h3.title').text().trim();
+	const episode = new Episode();
+	episode.image = PageInfo.url + $(element).find('figure.fa-play-circle img').attr('src');
+	episode.url   = PageInfo.url + $(element).find('article.episode a').attr('href');
+	
     for (let i = title.length - 1; i >= 0; i--) {
         if (title[i] == ' ') {
-            return { 
-                name: title.substring(0, i).trim(),  
-                episode: parseInt(title.substring(i + 1, title.length)),
-            };
+			episode.name   = title.substring(0, i).trim();
+			episode.number = parseInt(title.substring(i + 1, title.length));
+			break;
         }
     }
-    return null;
+	return episode;
 }
 
 /**
@@ -131,26 +95,17 @@ function getEpisodeInfo(title) {
  * @returns 
  */
 async function getLastEpisodes() {
+	let episodes = [];
     try {
-        let episodes = [];
         const $ = cheerio.load((await axios.get(PageInfo.url)).data);
-        $('div.container section ul.episodes li').each(async (i, element) => {
-            const episode = getEpisodeInfo($(element).find('h3.title').text().trim());
-            const url_episode = $(element).find('article.episode a').attr('href');
-            episodes.push({
-                name:    episode.name,
-                image:   $(element).find('figure.fa-play-circle img').attr('src'),
-                url:     url_episode,
-                episode: episode.episode,
-                video:   await getVideoServers(url_episode)
-            });
-            console.log(episodes[episodes.length - 1]);
-        });
-        return episodes;
+		const elements = $('div.container section ul.episodes li').children();
+		for (let i = 0; i < elements.length; i++) {
+			episodes.push(getEpisode($, elements[i]));
+		}
     } catch (error) {
         console.log(error);
     }
-    return null;
+    return episodes;
 }
 
 /**
@@ -158,7 +113,7 @@ async function getLastEpisodes() {
  * @param {*} $ 
  * @param {*} elements 
  */
-function getAnimeGenres($, elements) {
+function getGenres($, elements) {
     let genres = [];
     elements.each((i, element) => {
         genres.push($(element).find('a').text().trim());
@@ -168,27 +123,97 @@ function getAnimeGenres($, elements) {
 
 /**
  * 
- * @param {*} section movies, ovas and onas
- * @returns 
+ * @param {*} $
+ * @returns {Object}
  */
-async function getSectionContents(section) {
+function getScriptAnimeInfo($) {
+	'use strict'
+	try {
+		const script = $($('script').get().pop()).text().trim();
+		eval?.(script.substring(0, script.indexOf('$(document)')));
+        eval(script.substring(0, script.indexOf('$(document)')));
+		return {
+			// An object with the variables initialized in the script is returned.
+			info: globalThis.anime_info,
+			episodes: globalThis.episodes
+		}
+	} catch(error) {
+		
+	}
+	return null;
+}
+
+/**
+ * 
+ * @param {string} url anime url
+ * @returns {Anime}
+ */
+async function getAnime(url) {
+	// ignore property alt_name
+	const $ = cheerio.load((await axios.get(url)).data);
+	const data       = getScriptAnimeInfo($);
+	// It is possible that the object returned by the getScriptAnimeInfo function is null.
+	if (data === null) return null;
+	
+	const anime      = new Anime();
+    anime.name       = $('div.container h1.title').text();
+    anime.url        = url;
+	//anime.year       = parseInt($('div.meta span.year').text().trim());
+	anime.year       = new Date(data.info[3]).getFullYear();
+    anime.synopsis   = $('p.sinopsis').text().trim();
+    anime.genres     = getGenres($, $('div.container p.genres span'));
+    anime.image      = new Image(PageInfo.url + $('div.container div.thumb figure img').attr('src'), 
+								 PageInfo.url + $('figure.backdrop').attr('src'));
+    anime.active     = $('div.thumb a.status').text() === 'En emision';
+	anime.episodes   = await getAnimeEpisodes(data);
+	anime.chronology = getAnimeChronology($);
+	return anime;
+}
+
+
+/**
+ * finished!
+ */
+async function getLastAnimes() {
     try {
-        let contents = [];
+        let animes = [];
         const $ = cheerio.load((await axios.get(PageInfo.url)).data);
-        $(`div.container div.latest section.${ section } ul.list-unstyled li`).each((i, element) => {
-            contents.push({
-                name:        $(element).find('h3.title').text().trim(),
-                image:       $(element).find('figure.fa-play-circle img').attr('src'),
-                url:         $(element).find('article.anime a').attr('href'),
-                description: $(element).find('p.description').text().trim(),
-                genres:      getAnimeGenres($, $(element).find('p.genres span'))
-            });
-        });
-        return contents;
+		const elements = $('div.container section ul.list-unstyled.row li').children();
+		for (let i = 0; i < elements.length; i++) {
+            const anime_url = $(elements[i]).find('article.anime a').attr('href');
+            if (anime_url != undefined) {
+				const anime = await getAnime(PageInfo.url + anime_url);
+				if (anime !== null) {
+					animes.push(await getAnime(PageInfo.url + anime_url));
+				}
+            }
+		}
+        return animes;
     } catch (error) {
         console.log(error);
     }
-    return null;
+    return [];
+}
+
+
+/**
+ * 
+ * @param {*} section movies, ovas and onas
+ * @returns {Anime[]}
+ */
+async function getSectionContents(section) {
+	let animes = [];
+    try {
+        const $ = cheerio.load((await axios.get(PageInfo.url)).data);
+		const elements = $(`div.container div.latest section.${section} ul.list-unstyled li`).children();
+		for (let i = 0; i < elements.length; i++) {
+			animes.push(await getAnime(PageInfo.url + $(elements[i])
+				.find('article.anime a').attr('href')));
+		}
+    } catch (error) {
+        console.log(error);
+    }
+    return animes;
 }
 
 async function getLastMovies() {
@@ -203,6 +228,6 @@ async function getLastOnas() {
     return await getSectionContents('onas');
 }
 
-await getLastEpisodes();
+console.log(await getLastAnimes());
 
 export default { getLastEpisodes, getLastAnimes, getLastMovies, getLastOvas, getLastOnas };
