8fa20d44cd30c639e594bd1e2655736e2ef6ef77
Jorge Durón <duronj08@gmail.com>
2023-02-18 18:34:50 -0600

    add: monoschinos routes

--- src/routes/v1/anime/monoschinos/monoschinosRoutes.js
+++ src/routes/v1/anime/monoschinos/monoschinosRoutes.js
@@ -1,5 +1,7 @@
 import {Router} from 'express';
-import g from '../../../../scraper/sites/anime/monoschinos/Page.js'
+import g from '../../../../scraper/sites/anime/monoschinos/Page.js';
+import e from '../../../../scraper/sites/anime/monoschinos/Page.js';
+import f from '../../../../scraper/sites/anime/monoschinos/Page.js'
 const r = Router();
 
 r.get('/anime/monoschinos/name/:name', (req, res) => {
@@ -8,5 +10,16 @@ r.get('/anime/monoschinos/name/:name', (req, res) => {
         res.send(f)
     })
 })
-
+r.get('/anime/monoschinos/servers/:episode', (req, res) => {
+    const {episode} = req.params;
+    e.getEpisodeServers(episode).then(f =>{
+        res.send(f)
+    })
+})
+r.get('/anime/monoschinos/search', (req, res) => {
+    const {category, genre, year, letter} = req.query;
+    f.getFilterAnime(category, genre, year, letter).then(f => {
+        res.send(f)
+    })
+})
 export default r
