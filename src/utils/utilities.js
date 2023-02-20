4bd9cbf1a13853ac54ebd6197e0bb6b830e39364
Zukaritasu <77790801+Zukaritasu@users.noreply.github.com>
2023-02-19 20:57:48 -0400

    Added a file containing functions that perform specific tasks to reduce source code

---
+++ src/utils/utilities.js
@@ -0,0 +1,6 @@
+export default {
+    /** Returns true if argument is different from null and undefined */
+    isUsableValue: function(value) {
+        return value != null && value != undefined;
+    }
+}
