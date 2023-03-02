f8e34bb6eb0ffb80b825f9d620c270f992a6b960
koikiss-dev <duronj08@gmail.com>
2023-03-01 21:07:39 -0600

    monoschinos's route updated

--- src/routes/v1/anime/monoschinos/monoschinosRoutes.js
+++ src/routes/v1/anime/monoschinos/monoschinosRoutes.js
@@ -1,24 +1,30 @@
 import {Router} from 'express';
 import g from '../../../../scraper/sites/anime/monoschinos/Page.js';
 import e from '../../../../scraper/sites/anime/monoschinos/Page.js';
-import f from '../../../../scraper/sites/anime/monoschinos/Page.js'
+import f from '../../../../scraper/sites/anime/monoschinos/filter.js'
 const r = Router();
 
+//anime info
 r.get('/anime/monoschinos/name/:name', (req, res) => {
     const {name} = req.params;
-    g.getAnime(name).then(f => {
+    g.getAnime(`https://monoschinos2.com/anime/${name}`).then(f => {
         res.send(f)
     })
 })
-r.get('/anime/monoschinos/servers/:episode', (req, res) => {
-    const {episode} = req.params;
-    e.getEpisodeServers(episode).then(f =>{
+
+//get episode servers
+r.get('/anime/monoschinos/name/:name/episode/:ep', (req, res) => {
+    const {name, ep} = req.params;
+    //https://monoschinos2.com/ver/mou-ippon-episodio-8
+    e.getEpisodeServers(`https://monoschinos2.com/ver/${name}-episodio-${ep}`).then(f =>{
         res.send(f)
     })
 })
+
+//filter anime by params
 r.get('/anime/monoschinos/search', (req, res) => {
     const {category, genre, year, letter} = req.query;
-    f.getFilterAnime(category, genre, year, letter).then(f => {
+    f.filter(category, genre, year, letter).then(f => {
         res.send(f)
     })
 })
