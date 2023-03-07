64dc9467d6641566a1653ee68b9cbf3b57e3b1fd
Jorge Durón <48816029+koikiss-dev@users.noreply.github.com>
2023-03-07 00:08:05 -0600

    Corrections

--- src/types/anime.d.ts
+++ src/types/anime.d.ts
@@ -6,7 +6,7 @@ export interface Base {
 export interface Anime<S, E> extends Base {
   alternative_name?: string[];
   synopsis: S[]; //use Synopsis interface
-  link: `/anime/${string}/name/${string}`;//paramas -> provider's name & name of the anime -> /anime/flv/name/one-piece-tv
+  link: `/anime/${string}/name/${string}`;//params -> provider's name & name of the anime -> /anime/flv/name/one-piece-tv
   image: string;
   banner?: string;
   views?: number;
@@ -26,11 +26,11 @@ export interface Synopsis<C> {
 }
 export interface InfoCronology extends Base {
   image?: string;
-  link: `/anime/${string}/name/${string}`;//paramas -> provider's name & name of the anime -> /anime/flv/episode/one-piece-tv
+  link: `/anime/${string}/name/${string}`;//params -> provider's name & name of the anime -> /anime/flv/episode/one-piece-tv
 }
 
 export interface Episodes extends Base {
-  link: `/anime/${string}/episode/${string}`;//paramas -> provider's name & episode of the anime -> /anime/flv/episode/one-piece-tv-20;
+  link: `/anime/${string}/episode/${string}`;//params -> provider's name & episode of the anime -> /anime/flv/episode/one-piece-tv-20;
   number: `Episode ${number}`;
   image?: string;
 }
