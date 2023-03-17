88ac77f8f9feb703a1d1edc9f5fe162a2a93071f
Zukaritasu <77790801+Zukaritasu@users.noreply.github.com>
2023-03-17 16:07:59 -0400

    Added new schema providers

---
+++ src/types/episode.ts
@@ -0,0 +1,51 @@
+/***********************************************************************
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+//Spanish Providers - TypeScript version
+
+/**
+ * This interface only puts the server name where host episode, 
+* and url to that episode
+*
+ * @author Mawfyy 
+ */
+export interface IEpisodeServer {
+  name: string;
+  url: string;
+}
+
+/**
+ * Specify the structure episode, in the first url field is anime's name
+ * and the second is the episode number
+ * 
+ * @author Mawfyy 
+ */
+export interface IEpisode {
+  name: string;
+  url: `/anime/${string}/episode/${string | number}`;
+  number: number;
+  servers?: IEpisodeServer[];
+  image: string;
+}
+
+
+
+export class EpisodeServer implements IEpisodeServer {
+  name: string;
+  url: string;
+}
+
+export class Episode implements IEpisode {
+  name: string;
+  url: `/anime/${string}/episode/${string | number}`;
+  number: number;
+  servers?: IEpisodeServer[] = [];
+  image: string;
+}
---
+++ src/types/search.ts
@@ -0,0 +1,43 @@
+
+//Spanish Providers - TypeScript version
+
+/*
+ * Anime search helpers, use them with you scrapping by filter (searching..), 
+ * this format help you how you can return 
+ * theses results
+ *
+ * @author Mawfyy
+ */
+
+export interface IAnimeSearch {
+  name: string;
+  image: string;
+  url: `/anime/${string}/name/${string}`; // API url
+  type?: string;
+}
+
+export interface IPageNavigation {
+  count?: number;
+  current?: number;
+  next?: number;
+  hasNext?: boolean
+}
+
+export interface IResultSearch {
+  nav?: IPageNavigation;
+  results: IAnimeSearch[];
+}
+
+export class AnimeSearch implements IAnimeSearch {
+  name: string;
+  image: string;
+  url: `/anime/${string}/name/${string}`; // API url
+  type?: string;
+}
+
+export class ResultSearch implements IResultSearch {
+  nav?: IPageNavigation;
+  results: IAnimeSearch[] = [];
+}
+
+/** end */
