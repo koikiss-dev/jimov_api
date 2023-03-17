e6dc2b2207ec98cad59ff985678f3f06eb575ff5
Jorge Durón <48816029+koikiss-dev@users.noreply.github.com>
2023-03-17 22:51:00 +0000

    AnimeflvRoutes with typescript

---
+++ src/routes/v1/anime/animeflv/AnimeflvRoutes.ts
@@ -0,0 +1,63 @@
+import { Router } from "express";
+import { AnimeFlv } from "../../../../scraper/sites/anime/animeflv/AnimeFlv";
+import {
+  Genres,
+  TypeAnimeflv,
+  StatusAnimeflv,
+  OrderAnimeflv,
+} from "src/scraper/sites/anime/animeflv/animeflv_helper";
+const r = Router();
+
+
+//anime info
+r.get("/anime/flv/name/:name", async (req, res) => {
+  try {
+    const { name } = req.params;
+    const flv = new AnimeFlv();
+    const animeInfo = await flv.GetAnimeInfo(name);
+    res.send({
+      data: [{ animeInfo }],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+//episode servers
+r.get("/anime/flv/episode/:episode", async (req, res) => {
+  try {
+    const { episode } = req.params;
+    const flv = new AnimeFlv();
+    const animeInfo = await flv.GetEpisodeServers(episode);
+    res.send({
+      data: [{ animeInfo }],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+ 
+//filter
+r.get("/anime/flv/filter", async (req, res) => {
+  try {
+    const gen = req.query.gen as Genres;
+    const date = req.query.date as string;
+    const type = req.query.type as TypeAnimeflv;
+    const status = req.query.status as StatusAnimeflv;
+    const ord = req.query.ord as OrderAnimeflv;
+    const page = req.query.page as unknown as number;
+
+    const flv = new AnimeFlv();
+    const animeInfo = await flv.Filter(gen, date, type, status, ord, page);
+    res.send({
+      data: [{ animeInfo }],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+export default r;
