baf9dd6023374d6c7130d617c0c059efbfe90020
Jorge Durón <duronj08@gmail.com>
2023-04-07 20:31:50 +0000

    build: update

--- build/index.js
+++ build/index.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var morgan_1 = __importDefault(require("morgan"));
-var body_parser_1 = __importDefault(require("body-parser"));
 var app_1 = __importDefault(require("./routes/app"));
 var AnimeflvRoutes_1 = __importDefault(require("./routes/v1/anime/animeflv/AnimeflvRoutes"));
 var AnimeLatinoHDRoutes_1 = __importDefault(require("./routes/v1/anime/animelatinohd/AnimeLatinoHDRoutes"));
@@ -14,15 +13,10 @@ var app = (0, express_1.default)();
 var port = process.env.PORT || 3000;
 app.use(app_1.default);
 //config
-app.set("json spaces", 2);
-/*middleware*/
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, morgan_1.default)("dev"));
-app.use(body_parser_1.default.urlencoded({ extended: false }));
-app.use(body_parser_1.default.json());
-/*middleware*/
-/*headers */
-app.use((0, helmet_1.default)()); //segurity
-/*headers */
+app.use((0, helmet_1.default)());
 //routes
 /*animeflv*/
 app.use(AnimeflvRoutes_1.default);
