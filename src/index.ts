adabd88e8889d573d3f5b21b9e74af726cd057a5
Jorge Durón <duronj08@gmail.com>
2023-04-09 06:25:20 +0000

    feat(api)!: new routes. Gogoanime 95%, Monoschinos 90%

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import index from "./routes/app";
 import providersList from './routes/providers'
 import flv from "./routes/v1/anime/animeflv/AnimeflvRoutes";
 import latinhd from "./routes/v1/anime/animelatinohd/AnimeLatinoHDRoutes";
+import gogoanime from './routes/v1/anime/gogoanime/GogoAnimeRoute'
+import monoschinos from './routes/v1/anime/monoschinos/MonosChinosRoute'
 import helmet from "helmet";
 
 const app = express();
@@ -20,13 +22,16 @@ app.use(helmet());
 
 //routes
 
-/*animeflv*/
+/*anime*/
 app.use(flv);
 app.use(latinhd);
-/*animeflv*/
+app.use(gogoanime)
+app.use(monoschinos)
+/*anime*/
 
 
 //init
 app.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port} listo para trabajar :)`);
 });
+
