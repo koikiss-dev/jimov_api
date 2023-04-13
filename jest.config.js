ab370161dc58fed052f7ce402a45b139dd6c8504
Jorge Durón <duronj08@gmail.com>
2023-04-13 00:30:55 +0000

    refactor!: update jest config and delete files

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    roots: [
-      "<rootDir>/src",
-    ],
-    testMatch: [
-      "**/__tests__/**/*.+(ts|tsx|js)",
-      "**/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
-    transform: {
-      "^.+\\.(ts|tsx)$": "ts-jest"
+  roots: [
+    "<rootDir>/src",
+  ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
     },
-    collectCoverageFrom: [
-      "**/*.{js,jsx,ts,tsx}",
-      "!**/*.d.ts",
-      "!**/node_modules/**",
-    ],
-    globals: {
-      "ts-jest": {
-        tsconfig: "tsconfig.json",
-      },
-    },
-   }
+  },
+ }
