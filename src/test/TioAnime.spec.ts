d6e6541187cc1023da1d07a0a94801ab984ee404
zukaritasu <perezjhonlenno29@gmail.com>
2023-04-22 16:54:06 -0400

    Correction of errors in the code

--- src/test/TioAnime.spec.ts
+++ src/test/TioAnime.spec.ts
@@ -8,13 +8,13 @@ describe("TioAnime", () => {
     })
      it('should get anime info successfully', async () => {
       const animeInfo = await tioanime.getAnime('https://tioanime.com/anime/date-a-live');
-      if (animeInfo == null) return;
-
+     
       expect(animeInfo.name).toBe('Date A Live');
       expect(animeInfo.image.url).toContain('.jpg');
       expect(animeInfo.synopsis.length).toBeGreaterThan(0);
       expect(animeInfo.chronology?.length).toBeGreaterThan(0);
       expect(animeInfo.genres.length).toBeGreaterThan(0);
+      expect(animeInfo.episodes.length).toBeGreaterThan(0);
     });
     it('should filter anime successfully', async () => {
       const result = await tioanime.filter(["1"], ["accion"], { begin: 1950, end: 2023 }, 2, "recent");
