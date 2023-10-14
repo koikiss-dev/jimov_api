efd11cf27accdb26f46b3c563b3c1603ef0d0c3a
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2023-10-13 23:18:03 -0300

    raw

--- src/types/extractors.ts
+++ src/types/extractors.ts
@@ -1,6 +1,6 @@
-import * as cheerio from "cheerio";
+//import * as cheerio from "cheerio";
 import axios from "axios";
-import { UnPacked } from "./utils";
+//import { UnPacked } from "./utils";
 
 /**
  * 
@@ -10,18 +10,18 @@ import { UnPacked } from "./utils";
  * 
  * RequestBR Preload url needed by the request with cookies
  */
-
+axios.defaults.withCredentials = true
 export const filemoon = async (url: string) => {
     try {
         const Request = await axios.get(url,{headers:{"Accept":"*/*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
-        const $ = cheerio.load(Request.data)
+        //const $ = cheerio.load(Request.data)
 
-        const Buffer = btoa($("script").get().at(-1).children[0].data)
-        const UnBuffer = UnPacked(Buffer)
+        //const Buffer = btoa($("script").get().at(-1).children[0].data)
+        //const UnBuffer = UnPacked(Buffer)
 
-        const RequestBR = await eval(UnBuffer.slice(UnBuffer.indexOf("{sources:[{file:") + "{sources:[{file:".length, UnBuffer.indexOf("}],image:", 1)));
-        await axios.get(RequestBR,{headers:{"Accept":"*/*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
-        return RequestBR
+        //const RequestBR = await eval(UnBuffer.slice(UnBuffer.indexOf("{sources:[{file:") + "{sources:[{file:".length, UnBuffer.indexOf("}],image:", 1)));
+        //await axios.get(RequestBR,{headers:{"Accept":"*/*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
+        return Request.data
     } catch (error) {
         return error
     }
