3201c5ade42fddecc6d796c4d3884a72c41e87bb
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2023-10-13 23:45:01 -0300

    up

--- src/types/extractors.ts
+++ src/types/extractors.ts
@@ -10,10 +10,32 @@ import { UnPacked } from "./utils";
  * 
  * RequestBR Preload url needed by the request with cookies
  */
+
+fetch("https://filemoon.sx/e/5ehdd8cohg8r", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "es-419,es;q=0.9,es-ES;q=0.8,en;q=0.7,en-GB;q=0.6,en-US;q=0.5",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-ch-ua": "\"Microsoft Edge\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1"
+  },
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "include"
+});
 axios.defaults.withCredentials = true
 export const filemoon = async (_url: string) => {
-    try {
-        const Request = await axios.get("https://filemoon.sx/e/5ehdd8cohg8r",{headers:{"Cookie":"file_id=20298121; aff=18940","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60"}})
+
+        const Request = await axios.get("https://filemoon.sx/e/5ehdd8cohg8r")
         const $ = cheerio.load(Request.data)
 
         const Buffer = btoa($("script").get().at(-1).children[0].data)
@@ -22,8 +44,6 @@ export const filemoon = async (_url: string) => {
         //const RequestBR = await eval(UnBuffer.slice(UnBuffer.indexOf("{sources:[{file:") + "{sources:[{file:".length, UnBuffer.indexOf("}],image:", 1)));
         //await axios.get(RequestBR,{headers:{"Accept":"*/*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
         return UnBuffer
-    } catch (error) {
-        return error
-    }
+
 }
 
