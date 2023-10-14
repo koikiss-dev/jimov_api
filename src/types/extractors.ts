cd60fe02851360e1a2fe05a24d1e5da992408788
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2023-10-13 23:04:58 -0300

    up

--- src/types/extractors.ts
+++ src/types/extractors.ts
@@ -13,14 +13,14 @@ import { UnPacked } from "./utils";
 
 export const filemoon = async (url: string) => {
     try {
-        const Request = await axios.get(url,{headers:{"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
+        const Request = await axios.get(url,{headers:{"Accept":"*/*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
         const $ = cheerio.load(Request.data)
 
         const Buffer = btoa($("script").get().at(-1).children[0].data)
         const UnBuffer = UnPacked(Buffer)
 
         const RequestBR = await eval(UnBuffer.slice(UnBuffer.indexOf("{sources:[{file:") + "{sources:[{file:".length, UnBuffer.indexOf("}],image:", 1)));
-        axios.get(RequestBR)
+        await axios.get(RequestBR,{headers:{"Accept":"*/*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
         return RequestBR
     } catch (error) {
         return error
