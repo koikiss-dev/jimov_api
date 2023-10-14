a0378f7d19b49ef8045d16bc874aaa295e67ffca
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2023-10-14 14:47:37 -0300

    UP

--- src/types/extractors.ts
+++ src/types/extractors.ts
@@ -1,6 +1,6 @@
-import * as cheerio from "cheerio";
+//import * as cheerio from "cheerio";
 import axios from "axios";
-import { UnPacked } from "./utils";
+//import { UnPacked } from "./utils";
 
 /**
  * 
@@ -36,14 +36,14 @@ axios.defaults.withCredentials = true
 export const filemoon = async (_url: string) => {
 
         const Request = await axios.get("https://filemoon.sx/e/5ehdd8cohg8r")
-        const $ = cheerio.load(Request.data)
+        //const $ = cheerio.load(Request.data)
 
-        const Buffer = btoa($("script").get().at(-1).children[0].data)
-        const UnBuffer = UnPacked(Buffer)
+        //const Buffer = btoa($("script").get().at(-1).children[0].data)
+        //const UnBuffer = UnPacked(Buffer)
 
         //const RequestBR = await eval(UnBuffer.slice(UnBuffer.indexOf("{sources:[{file:") + "{sources:[{file:".length, UnBuffer.indexOf("}],image:", 1)));
         //await axios.get(RequestBR,{headers:{"Accept":"*/*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
-        return UnBuffer
+        return Request
 
 }
 
