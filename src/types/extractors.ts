eb3bfcef2301ad1d2c65cb5231d1451b14d350a3
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2023-10-14 17:02:30 -0300

    up

--- src/types/extractors.ts
+++ src/types/extractors.ts
@@ -1,7 +1,6 @@
 //import * as cheerio from "cheerio";
 import axios from "axios";
 //import { UnPacked } from "./utils";
-import { webkit } from 'playwright';
 
 /**
  * 
@@ -15,18 +14,16 @@ import { webkit } from 'playwright';
 
 axios.defaults.withCredentials = true
 export const filemoon = async (_url: string) => {
-  try {
-    const browser = await webkit.launch();
-    const page = await browser.newPage();
-    await page.goto('https://filemoon.sx/e/irin5v8w3kxe');
 
+        const Request = await axios.get("https://filemoon.sx/e/5ehdd8cohg8r")
+        //const $ = cheerio.load(Request.data)
 
- 
-    const data = await page.content()
-    await browser.close();
-    return data
-  } catch (error) {
-    return error
-  }
+        //const Buffer = btoa($("script").get().at(-1).children[0].data)
+        //const UnBuffer = UnPacked(Buffer)
+
+        //const RequestBR = await eval(UnBuffer.slice(UnBuffer.indexOf("{sources:[{file:") + "{sources:[{file:".length, UnBuffer.indexOf("}],image:", 1)));
+        //await axios.get(RequestBR,{headers:{"Accept":"*/*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.55"}})
+        return Request
+
 }
 
