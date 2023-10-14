b8cac1271791acff912da13b3f4b119207f6449f
barrientosvctor <vibarrientosp@gmail.com>
2023-10-13 20:49:14 -0500

    feat(route): get manga chapters for manganato

--- src/routes/v1/manga/manganato/ManganatoRoutes.ts
+++ src/routes/v1/manga/manganato/ManganatoRoutes.ts
@@ -12,4 +12,10 @@ router.get("/manga/manganato/title/:id", async (req, res) => {
   return res.status(200).send(result);
 });
 
+router.get("/manga/manganato/chapter/:id", async (req, res) => {
+  const result = await manganato.GetMangaChapters(req.params.id as unknown as string, req.query.num as unknown as number);
+
+  return res.status(200).send(result);
+});
+
 export default router;
