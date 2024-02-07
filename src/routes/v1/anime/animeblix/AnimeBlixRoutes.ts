f61f0857c493cfa7715b8c728bec42d8b809fedc
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2024-02-07 04:36:06 -0300

    test rare

--- src/routes/v1/anime/animeblix/AnimeBlixRoutes.ts
+++ src/routes/v1/anime/animeblix/AnimeBlixRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AnimeBlix } from "../../../../scraper/sites/anime/AnimeBlix/AnimeBlix";
+import { AnimeBlix } from "../../../../scraper/sites/anime/animeBlix/AnimeBlix";
 const Anime = new AnimeBlix();
 const router = Router();
 
