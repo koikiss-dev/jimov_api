b0c63c9d4f21731bde76049844b65343cb699386
barrientosvctor <vibarrientosp@gmail.com>
2024-05-26 12:12:08 -0500

    feat(MangaScraperModel): add extension to new manga interfaces

--- src/models/MangaScraperModel.ts
+++ src/models/MangaScraperModel.ts
@@ -1,6 +1,6 @@
-import type { IMangaResult, Manga, MangaChapter, MangaVolume } from "@animetypes/manga";
+import type { MangaMedia, MangaChapter, MangaVolume, IMangaResult } from "@animetypes/manga";
 import { BaseScraperModel } from "./BaseScraperModel";
 
-export abstract class MangaScraperModel extends BaseScraperModel<Manga, IMangaResult> {
+export abstract class MangaScraperModel extends BaseScraperModel<MangaMedia, IMangaResult> {
   public abstract GetMangaChapters(...args: unknown[]): Promise<MangaChapter | MangaVolume>
 }
