319266aee1041bf853782da915872580ea4cf33a
barrientosvctor <vibarrientosp@gmail.com>
2024-05-26 12:11:44 -0500

    feat(AnimeScraperModel): add extension to new anime interfaces

--- src/models/AnimeScraperModel.ts
+++ src/models/AnimeScraperModel.ts
@@ -1,8 +1,8 @@
-import type { Anime } from "../types/anime";
-import type { IAnimeSearch } from "../types/search";
-import type { Episode } from "../types/episode";
+import type { AnimeMedia } from "@animetypes/anime";
+import type { Episode } from "@animetypes/episode";
+import type { IAnimeResult } from "@animetypes/search";
 import { BaseScraperModel } from "./BaseScraperModel";
 
-export abstract class AnimeScraperModel extends BaseScraperModel<Anime, IAnimeSearch> {
+export abstract class AnimeScraperModel extends BaseScraperModel<AnimeMedia, IAnimeResult> {
   public abstract GetEpisodeServers(...args: unknown[]): Promise<Episode>;
 }
