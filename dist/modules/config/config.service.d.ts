22ff9924cfa9215a72b87592b95748a5e3eb5e1e
yako <duronj08@gmail.com>
2025-05-01 19:51:48 -0600

    delete:dist

--- dist/modules/config/config.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { EnvironmentVariables } from 'src/lib/utils/validateShema';
-export declare class AppConfigService {
-    private readonly config;
-    constructor(config: ConfigService<EnvironmentVariables>);
-    get<K extends keyof EnvironmentVariables>(value: K): import("@nestjs/config").PathValue<EnvironmentVariables, K> | undefined;
-}
